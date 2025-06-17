a6e4be511f473900cf897611a37b3c551f3fd33d
Devendra-03 <bdevendra692@gmail.com>
2025-06-17 19:37:00 +0530

    Add files via upload

---
+++ FabonacciWithoutDP.java
@@ -0,0 +1,13 @@
+package java11_6_2025;
+
+public class FabonacciWithoutDP {
+	public static int fib (int n) {
+		if(n<=1)
+			return n;
+		return fib(n-1) + fib(n-2);
+	}
+	public static void main(String[] args) {
+		int n=6;
+		System.out.println("Fibonacci of "+ n +" is "+fib(n));
+	}
+}
